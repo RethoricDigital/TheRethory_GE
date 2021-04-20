ef639aa9b22e9ba91b7cb30b08a5ab9425cbb3be
Emanuel <emanuel.s456@gmail.com>
2021-04-20 15:48:27 -0500

    Cambio4

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,7 +8,7 @@
 							<li><a href="#"><i class="fab fa-twitter"></i></a></li>
 							<li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
 							<li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-							<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+							<li><a href="#"><i class="fab fa-instagram"></i></a></li>s
 						</ul>
 					</div>
 				</div>
